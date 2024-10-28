be4ab8e762fa9aafb84f9e349cf600c29936cc0f
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2024-10-28 19:38:59 +0000

    Make the snow fill the screen

--- apps/issy-secret-santa/app/features/guessing-game/finish-screen.tsx
+++ apps/issy-secret-santa/app/features/guessing-game/finish-screen.tsx
@@ -1,10 +1,19 @@
 import Confetti from 'react-confetti';
 import { motion } from 'framer-motion';
+import { useWindowSize } from '@uidotdev/usehooks';
 
 export const FinishScreen = () => {
+    const { width, height } = useWindowSize();
+
     return (
         <div className="flex h-full w-full flex-col items-center justify-center bg-red-600 p-4 text-white">
-            <Confetti colors={['#FFF']} friction={1} gravity={0.01} />
+            <Confetti
+                colors={['#FFF']}
+                friction={1}
+                gravity={0.01}
+                width={width ?? 0}
+                height={height ?? 0}
+            />
             <motion.div
                 className="text-center text-3xl"
                 initial={{ opacity: 0 }}
